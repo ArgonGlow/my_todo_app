de77d277154864fba8c989d07878e57925a9afe9
ArgonGlow <koen2702@gmail.com>
2022-05-26 17:59:26 +0200

    local storage v1 done

--- app.js
+++ app.js
@@ -1,22 +1,47 @@
 const taskList = document.getElementById('task-list')
 const confirmBtn = document.getElementById('add')
+const deleteBtn = document.getElementById('delete')
 const newTaskField = document.getElementById('add-task')
 
-console.log(taskList)
+//console.log(taskList)
 
 confirmBtn.addEventListener("click", readNewTask)
 
 //console.log(localStorage[7].innerHTML);
 //taskList.appendChild(localStorage.getItem(7))
 
-localStorage.clear
+//localStorage.clear
 
-// Object.keys(localStorage).forEach(function(key){
-//     console.log(localStorage.getItem(key));
-//     NewListItem(key, false)
-//  });
+loadTasksFromStorage()
+
+function loadTasksFromStorage() {
+
+    let taskArray = []
+
+    Object.keys(localStorage).forEach(function(key){
+        // if it is a task > starts with i (id7, id8, etc.)
+        if (key[0] == 'i') {
+            //console.log(localStorage.getItem(key));
+            let jsonString = localStorage.getItem(key);
+            let jsonObject = JSON.parse(jsonString);
+            taskArray[key] = jsonObject.text
+            //console.log(jsonString)
+            //console.log(jsonObject)
+        }
+        //console.log(objtest)
+    });
+
+    taskArray.sort()
+    Object.keys(taskArray).forEach(key => {
+        console.log(taskArray)
+        NewListItem(taskArray[key], false, key)
+    })
+}
+
+
 
 function storeTask(key, task) {
+    console.log('storeTask')
     let obj = new taskObj(task);
     let jsonObject = JSON.stringify(obj);
     localStorage.setItem(key, jsonObject);
@@ -24,6 +49,7 @@ function storeTask(key, task) {
 }
 
 function getTask(key) {
+    console.log('getTask')
     let jsonString = localStorage.getItem(key);
     let obj = JSON.parse(jsonString);
     console.log(obj.text)
@@ -33,6 +59,16 @@ function taskObj(task) {
     this.text = task;
 }
 
+
+function deleteTask(button) {
+    let parentID = button.parentElement.id;
+    //console.log(deleteBtn)
+    console.log("del-parent: " + parentID);
+    button.parentElement.remove();
+    localStorage.removeItem(`id${parentID}`)
+    
+}
+
 function readNewTask() {
     const newInput = newTaskField.value
     newTaskField.value = ''
@@ -40,8 +76,9 @@ function readNewTask() {
 
 }
 
-function NewListItem(newTask, isNew) {
-    
+function NewListItem(newTask, isNew, key) {
+    console.log('NewListItem')
+
     const newDiv = document.createElement("div")
     const newItem = document.createElement("li")
     const newButton = document.createElement("button")
@@ -51,14 +88,17 @@ function NewListItem(newTask, isNew) {
     
     let itemID = String(LastItemID() + 1)
 
-    console.log(itemID)
+    if (!isNew) {itemID = String(key.slice(2))}
+
+    console.log('next ID: ' + itemID)
 
     // <div class="list-item">
     newDiv.setAttribute("class", "list-item")
     newDiv.setAttribute("id", itemID)
     // <button class="del-btn" id="delete" >
     newButton.setAttribute("class", "del-btn")
-    newButton.setAttribute("id", "delete")
+    newButton.setAttribute("id", itemID)
+    newButton.addEventListener("click", () => {deleteTask(newButton)})
     // <i class="fa-solid fa-trash">
     newIcon.setAttribute("class", "fa-solid fa-trash")
     // <input type="checkbox" name="completed" id="6" />
@@ -72,8 +112,9 @@ function NewListItem(newTask, isNew) {
     newDiv.appendChild(newItem)
     newDiv.appendChild(newButton)
 
-    console.log(isNew)
-    if(!isNew) storeTask(itemID, newTask);
+    // console.log(isNew)
+    console.log(`id${itemID}`)
+    if(isNew) storeTask(`id${itemID}`, newTask);
 
     // add new item to list
     taskList.appendChild(newDiv)
