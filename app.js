a33a392260c0c258a42d000a3cc1ab38fdeb20b9
ArgonGlow <koen2702@gmail.com>
2022-05-26 16:19:54 +0200

    playing with localStorage

--- app.js
+++ app.js
@@ -6,14 +6,41 @@ console.log(taskList)
 
 confirmBtn.addEventListener("click", readNewTask)
 
+//console.log(localStorage[7].innerHTML);
+//taskList.appendChild(localStorage.getItem(7))
+
+localStorage.clear
+
+// Object.keys(localStorage).forEach(function(key){
+//     console.log(localStorage.getItem(key));
+//     NewListItem(key, false)
+//  });
+
+function storeTask(key, task) {
+    let obj = new taskObj(task);
+    let jsonObject = JSON.stringify(obj);
+    localStorage.setItem(key, jsonObject);
+    getTask(key);
+}
+
+function getTask(key) {
+    let jsonString = localStorage.getItem(key);
+    let obj = JSON.parse(jsonString);
+    console.log(obj.text)
+}
+
+function taskObj(task) {
+    this.text = task;
+}
+
 function readNewTask() {
     const newInput = newTaskField.value
     newTaskField.value = ''
-    NewListItem(newInput)
+    NewListItem(newInput, true)
 
 }
 
-function NewListItem(newTask) {
+function NewListItem(newTask, isNew) {
     
     const newDiv = document.createElement("div")
     const newItem = document.createElement("li")
@@ -45,13 +72,16 @@ function NewListItem(newTask) {
     newDiv.appendChild(newItem)
     newDiv.appendChild(newButton)
 
+    console.log(isNew)
+    if(!isNew) storeTask(itemID, newTask);
+
     // add new item to list
     taskList.appendChild(newDiv)
 }
 
 function LastItemID() {
     let listItems = taskList.children
-    console.log(taskList.children)
+    // console.log(taskList.children)
     let lastID = 0
     for (let i = 0; i < listItems.length; i++) {
         if (parseInt(listItems[i].id) > lastID) {
